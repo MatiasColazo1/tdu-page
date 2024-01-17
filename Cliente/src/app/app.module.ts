4e4d38cb429a1012e03f464b44fcd65629c1995e
kiri-1 <belenjesuslp@gmail.com>
2024-01-17 11:31:09 -0300

    creando cursos

--- Cliente/src/app/app.module.ts
+++ Cliente/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { CursosComponent } from './components/cursos/cursos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    BannerComponent
+    BannerComponent,
+    CursosComponent
   ],
   imports: [
     BrowserModule,
