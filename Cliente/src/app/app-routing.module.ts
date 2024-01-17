e830ecd0e446bcb6a406f4a9d6c79e203de7bd74
kiri-1 <belenjesuslp@gmail.com>
2024-01-17 11:48:15 -0300

    creando home

--- Cliente/src/app/app-routing.module.ts
+++ Cliente/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { CursosComponent } from './components/cursos/cursos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'cursos', component:CursosComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Cliente/src/app/app.module.ts
+++ Cliente/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { CursosComponent } from './components/cursos/cursos.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     BannerComponent,
-    CursosComponent
+    CursosComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
