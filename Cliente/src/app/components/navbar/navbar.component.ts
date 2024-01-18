355773b9159f18f30e71bdf113e96979003c802f
matias <maty_colazo@hotmail.com>
2024-01-18 10:38:38 -0300

    background navbar

--- Cliente/src/app/components/navbar/navbar.component.ts
+++ Cliente/src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   isMenuOpen = false;
+  isScrolled = false;
+
+  ngOnInit(): void {
+    window.addEventListener('scroll', this.scroll, true); // third parameter
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  scroll(event: any): void {
+    this.isScrolled = window.scrollY > 100; // Ajusta 100 a la distancia de scroll que prefieras
+  }
+
+  ngOnDestroy(): void {
+    window.removeEventListener('scroll', this.scroll, true);
+  }
 
   toggleMenu() {
       this.isMenuOpen = !this.isMenuOpen;
