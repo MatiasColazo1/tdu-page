ac207a266ee872c7ac24bf8964d2e825d36729d7
matias <maty_colazo@hotmail.com>
2024-01-18 20:15:28 -0300

    arreglos

--- Cliente/src/app/app-routing.module.ts
+++ Cliente/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { CursosComponent } from './components/cursos/cursos.component';
 import { ContactoComponent } from './components/contacto/contacto.component';
+import { SobremiComponent } from './components/sobremi/sobremi.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/inicio', pathMatch: 'full' },
   { path: 'inicio', component:HomeComponent },
   { path:'cursos', component:CursosComponent },
   { path:'contacto', component:ContactoComponent },
+  { path:'sobremi', component:SobremiComponent },
 ];
 
 @NgModule({
