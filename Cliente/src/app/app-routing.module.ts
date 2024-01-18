801f0df4c2392d9f444783099e53928e88593606
kiri-1 <belenjesuslp@gmail.com>
2024-01-18 19:13:07 -0300

    contacto

--- Cliente/src/app/app-routing.module.ts
+++ Cliente/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { CursosComponent } from './components/cursos/cursos.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/inicio', pathMatch: 'full' },
   { path: 'inicio', component:HomeComponent },
   { path:'cursos', component:CursosComponent },
+  { path:'contacto', component:ContactoComponent },
 ];
 
 @NgModule({
